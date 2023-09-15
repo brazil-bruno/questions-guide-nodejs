ee78599e514ed311064459c0a56358e0eb2dd6ef
brazil-bruno <bruno.brazil2022@gmail.com>
2023-09-15 14:26:43 -0300

    body-parser install and save method created

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
-  
   res.render("index");
 });
 
 app.get("/toask", (req, res) => {
   res.render("toask");
-})
+});
+
+app.post("/savequestion", (req, res) => {
+  var title = req.body.title;
+  var description = req.body.description
+  res.send("Form received! Title: " + title + " " + "Description: " + description);
+});
 
 app.listen(8080, () => {
   console.log("App is running...");
